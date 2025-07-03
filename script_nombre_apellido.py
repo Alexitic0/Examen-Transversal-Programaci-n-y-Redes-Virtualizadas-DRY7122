9a2adc8b1babdc6431785969aa7a106e1aa11c37
Alexitic0 <alex.vega@duocuc.cl>
2025-07-02 23:25:52 -0400

    Scripts Item 1

---
+++ script_nombre_apellido.py
@@ -0,0 +1,10 @@
+integrantes = [
+    "Diego Abrigo",
+    "Brian Castro",
+    "Joaquín Mella",
+    "Alexis Vega"
+]
+
+print("Integrantes del grupo:")
+for integrante in integrantes:
+    print(integrante)
